0452a76f39dc1f82ab8dcc326187b022a49f7c03
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-02-17 20:21:40 +0530

    Learning GSAP via mini Project

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 var tl = gsap.timeline();
 
-tl.to("#box1", {
-  x: 1200,
-  rotate: 360,
-  scale: 0.5,
-  duration: 2,
-  delay: 1,
+tl.from("#nav h3", {
+  y: -50,
+  opacity: 0,
+  delay: 0.4,
+  duration: 0.6,
+  stagger: 0.3,
 });
-tl.to("#box2", {
-  x: 1200,
-  rotate: 360,
-  scale: 0.5,
-  duration: 2,
-  delay: 1,
+
+tl.from("#main h1", {
+  x: -500,
+  opacity: 0,
+  duration: 0.8,
+  stagger: 0.3,
 });
-tl.to("#box3", {
-  x: 1200,
-  rotate: 360,
-  scale: 0.5,
-  duration: 2,
-  delay: 1,
+
+tl.from("img", {
+  x: 100,
+  rotate: 45,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.5,
 });
